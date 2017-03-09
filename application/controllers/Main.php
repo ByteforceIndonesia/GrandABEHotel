408cd7deb6a694c3afedb6065f677c9216023965
Jonathan Hosea <jonathan.hosea@me.com>
2017-03-09 08:45:09 +0700

    template

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -3,8 +3,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Main extends CI_Controller {
 
+	function __construct ()
+	{
+		parent::__construct();
+
+		$data = array (
+			'page_title' => 'Grand ABE Hotel Jayapura'
+			);
+	}
+
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->template->load('template', 'home', $data);
 	}
 }
