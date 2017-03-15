62874182cfb3ca66484572992cb1d1cd0c3a8a70
Jonathan Hosea <jonathan.hosea@me.com>
2017-03-15 13:18:14 +0700

    Frontend ud kelar minus media

--- application/views/rooms.php
+++ application/views/rooms.php
@@ -5,7 +5,7 @@
 			<div class="panelHeaderRooms">
 				<h1>Guest Rooms</h1>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque convallis, sem et accumsan convallis, sapien lectus consequat ante, ac placerat erat neque a nulla. Fusce pellentesque diam ante, at placerat velit vehicula gravida. Suspendisse pharetra ullamcorper laoreet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris vitae porttitor ex. Vestibulum ac tortor congue, pellentesque diam non, congue magna.
+					Grand ABE Hotel memiliki 98 kamar tamu, yang terdiri dari tipe SUPERIOR ROOM, DELUXE ROOM, FAMILY SUITE, EXECUTIVE SUITE dan PRESIDENT SUITE. Tersedia pilihan smoking dan non-smoking room.
 				</p>
 			</div>
 		</div>
@@ -19,11 +19,11 @@
 				<div class="imgRoom"></div>
 			</div>
 		</div>
-		<div class="col-lg-6">
+		<div class="col-lg-5">
 			<div class="headerPanelRoomText">
 				<h3>Superior Room</h3>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque convallis, sem et accumsan convallis, sapien lectus consequat ante, ac placerat erat neque a nulla. Fusce pellentesque diam ante, at placerat velit vehicula gravida. Suspendisse pharetra ullamcorper laoreet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris vitae porttitor ex. Vestibulum ac tortor congue, pellentesque diam non, congue magna.
+					Kamar tipe SUPERIOR memiliki luas 23 m2, tanpa jendela dan dilengkapi dengan air condition, sofa, TV 32” siaran lokal dan internasional, free Wi-Fi, telepon, mineral water, teh & kopi, hot & cold shower, perlengkapan mandi dan fasilitias lainnya. Kami memiliki 20 kamar tipe SUPERIOR.
 				</p>
 			</div>
 		</div>
@@ -35,11 +35,11 @@
 				<div class="imgRoom"></div>
 			</div>
 		</div>
-		<div class="col-lg-6">
+		<div class="col-lg-5">
 			<div class="headerPanelRoomText">
 				<h3>Deluxe Room</h3>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque convallis, sem et accumsan convallis, sapien lectus consequat ante, ac placerat erat neque a nulla. Fusce pellentesque diam ante, at placerat velit vehicula gravida. Suspendisse pharetra ullamcorper laoreet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris vitae porttitor ex. Vestibulum ac tortor congue, pellentesque diam non, congue magna.
+					Kamar tipe DELUXE memiliki luas 26 m2 dan dilengkapi dengan jendela, air condition, sofa, TV 32” siaran lokal dan internasional, free Wi-Fi, telepon, mineral water, teh & kopi, hot & cold shower, perlengkapan mandi dan fasilitias lainnya. Kami memiliki 64 kamar tipe DELUXE dengan pilihan twin bed dan double bed.
 				</p>
 			</div>
 		</div>
@@ -51,11 +51,11 @@
 				<div class="imgRoom"></div>
 			</div>
 		</div>
-		<div class="col-lg-6">
+		<div class="col-lg-5">
 			<div class="headerPanelRoomText">
 				<h3>Family Suit</h3>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque convallis, sem et accumsan convallis, sapien lectus consequat ante, ac placerat erat neque a nulla. Fusce pellentesque diam ante, at placerat velit vehicula gravida. Suspendisse pharetra ullamcorper laoreet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris vitae porttitor ex. Vestibulum ac tortor congue, pellentesque diam non, congue magna.
+					Kamar tipe FAMILY SUITE memiliki luas 30 m2, berkapasitas tiga orang dan dilengkapi dengan 2 tempat tidur tipe double bed dan twin bed, sofa, kulkas, jendela, air condition, TV 32” siaran lokal dan internasional, telepon, mineral water, teh & kopi, hot & cold shower, perlengkapan mandi dan fasilitias lainnya. Kami memiliki 4 kamar tipe FAMILY SUITE.
 				</p>
 			</div>
 		</div>
@@ -67,11 +67,12 @@
 				<div class="imgRoom"></div>
 			</div>
 		</div>
-		<div class="col-lg-6">
+		<div class="col-lg-5">
 			<div class="headerPanelRoomText">
-				<h3>Deluxe Room</h3>
+				<h3>Exceutive Suit</h3>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque convallis, sem et accumsan convallis, sapien lectus consequat ante, ac placerat erat neque a nulla. Fusce pellentesque diam ante, at placerat velit vehicula gravida. Suspendisse pharetra ullamcorper laoreet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris vitae porttitor ex. Vestibulum ac tortor congue, pellentesque diam non, congue magna.
+					Kamar tipe EXECUTIVE SUITE memiliki luas 48 m2, dan dilengkapi dengan double bed, sofa, kulkas, jendela, air condition, TV 32” siaran lokal dan internasional, telepon, safe deposit box, mineral water, teh & kopi, hot & cold shower, lemari, sofa  perlengkapan mandi dan fasilitias lainnya. Kami memiliki 8 kamar tipe EXECUTIVE SUITE.
+
 				</p>
 			</div>
 		</div>
@@ -83,11 +84,12 @@
 				<div class="imgRoom"></div>
 			</div>
 		</div>
-		<div class="col-lg-6">
+		<div class="col-lg-5">
 			<div class="headerPanelRoomText">
 				<h3>Presidential Suit</h3>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque convallis, sem et accumsan convallis, sapien lectus consequat ante, ac placerat erat neque a nulla. Fusce pellentesque diam ante, at placerat velit vehicula gravida. Suspendisse pharetra ullamcorper laoreet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris vitae porttitor ex. Vestibulum ac tortor congue, pellentesque diam non, congue magna.
+					Kamar tipe PRESIDENT SUITE memiliki luas 54 m2, dengan kamar tidur dan ruang tamu yang terpisah. Tersedia bar area dengan dapur kecil dan mini bar di dalam kamar, juga dilengkapi dengan double bed, sofa, kulkas, jendela, air condition, TV 42” siaran lokal dan internasional, telepon, safe deposit box, mineral water, teh & kopi, hot & cold shower, bathtub,  perlengkapan mandi dan fasilitias lainnya. 
+
 				</p>
 			</div>
 		</div>
