f01dd561249c1b4890d474e26724af4a1bd5aae2
Jonathan Hosea <jonathan.hosea@me.com>
2017-04-15 21:21:52 +0700

    benerin navbar lg

--- application/views/templates/Template.php
+++ application/views/templates/Template.php
@@ -28,7 +28,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <div class="wrapper">
 		<!-- navbar -->
 	<nav class="navbar navbar-toggleable-lg navbar-light bg-faded" id="navigation">
-	  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+	  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" id="toggleNav">
 	    <span class="navbar-toggler-icon"></span>
 	  </button>
 	  <a class="navbar-brand" href="#">GRAND ABE <strong>HOTEL</strong></a>
@@ -114,6 +114,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		    return -(this.element.clientHeight + 300)
 		  }
 	  };	  
+	});
+
+	$('#toggleNav').click(function(){
+		if($('#navbar').hasClass('show'))
+		{
+			$('#navigation').removeClass('opened');
+		}else
+		{
+			$('#navigation').addClass('opened');
+		}
 	});
 </script>
 </html>
