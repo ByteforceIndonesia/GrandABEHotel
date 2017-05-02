e0cbe69b776923a83ab5529ea8d296b6d7c47d41
Jonathan Hosea <jonathan.hosea@me.com>
2017-05-02 16:40:34 +0700

    AKHIRNYA LOCATION RAPIH SETELAH 4 JEM DI SBUX

--- application/views/location.php
+++ application/views/location.php
@@ -1,12 +1,49 @@
 <link rel="stylesheet" href="<?php echo base_url() . CSS_DIR . '/component.css'?>">
 <link rel="stylesheet" href="<?php echo base_url() . CSS_DIR . '/slick.css'?>">
 <link rel="stylesheet" href="<?php echo base_url() . CSS_DIR . '/slick-theme.css'?>">
+<link rel="stylesheet" href="<?php echo base_url() . CSS_DIR . '/magnific-popup.css'?>">
 <script src="<?php echo base_url() . JS_DIR . '/classie.js' ?>"></script>
 <script src="<?php echo base_url() . JS_DIR . '/modernizr.min.js' ?>"></script>
 <script src="<?php echo base_url() . JS_DIR . '/photostack.js' ?>"></script>
 <script src="<?php echo base_url() . JS_DIR . '/slick.min.js' ?>"></script>
+<script src="<?php echo base_url() . JS_DIR . '/jquery.magnific-popup.min.js' ?>"></script>
 
-<!--//DESIGN ini di tmpt gw terlalu kiri, ga kliatan jona -->
+<script>
+$(document).ready(function(){
+	
+	var popUpId = 0;
+
+	$('.locDes').each(function()
+	{
+		if($(this).html().length > 400)
+		{
+			var original = $(this).html();
+			var text = original;
+
+			text = text.substring(0, 400);
+			last = text.lastIndexOf(" ");
+			text = text.substring(0, last);
+
+			$(this).html(text + '... <a class="popupclick" id="#' + popUpId +'" href="#' + popUpId +'">Read More</a>');
+
+			$(this).parent().append('<div class="popup mfp-hide" id="'  + popUpId + '">' + original + '</div');
+
+			popUpId++;
+		}
+	});
+
+	for(var i=0; i<popUpId; i++)
+	{
+		$(".popupclick").magnificPopup({
+			type:'inline',
+			midClick: true
+		});
+	}
+
+});
+</script>
+
+
 <section id="whereAreWe">
 	<div class="row">
 		<div class="col-xl-5">
@@ -39,17 +76,16 @@
 	<?php
 		if(!empty($locations)){
 			foreach ($locations as $location) {
+				// if the string is longer than 50 chars
 				echo'
-				<div>
-					<div class="col-lg-1"></div>
-					<div class="col-lg-5">
+				<div class="row">
+					<div class="col-lg-6 panel">
 						<div class="touristLeft">
-							<h1>'.$location->name.'</h1>'.
-								$location->description.'
+							<h1>'.$location->name.'</h1>'. 
+							'<div class="locDes wordwrapper">'.$location->description.'</div>
 						</div>
 					</div>
-					<div class="col-lg-1"></div>
-					<div class="col-lg-5">
+					<div class="col-lg-6 panel">
 						<div class="imageTourist" 
 							 style="background:url(\''. base_url() .'assets/images/uploads/locations/' . $location->image.'\');
 							 ">&nbsp</div>
@@ -104,7 +140,7 @@
 	$(document).ready(function(){
 	  $('#touristAttraction').slick({
 	  	autoplay: true,
-  		autoplaySpeed: 2000,
+  		autoplaySpeed: 5000,
 	  });
 	});
 </script>
