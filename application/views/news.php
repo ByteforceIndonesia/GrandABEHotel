41c20c13216ae15c9fc97fb0bbbe6b6028640645
Jef_fry <jeffry24797@gmail.com>
2017-07-07 02:18:41 +0700

    news page

---
+++ application/views/news.php
@@ -0,0 +1,81 @@
+<section id="headerNews">
+	<div class="row">
+		<div class="col-lg-1">&nbsp</div>
+		<div class="col-lg-5">
+			<div class="panelHeaderNews">
+				<h1>News & Promotions</h1>
+			</div>
+		</div>
+	</div>
+</section>
+
+<center>
+	<section id="newsList">
+
+		<div class="row newsItem">
+			<div class="col-lg-3 newsImg">
+				<div class="imgContainer">
+					<img class="img-responsive" src="
+					<?php echo base_url().'assets/images/uploads/news/testNews.jpg'?>" 
+					width="100px" height="100px">
+				</div>
+			</div>
+			<div class="col-lg-9">
+				<div class="row newsTitle">
+					Test News Title
+				</div>
+				<div class="row postDate">
+					31 December 2017
+				</div>
+				<div class="row newsDesc">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tincidunt, sapien sed eleifend scelerisque, ipsum nibh sodales erat, sed feugiat....</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="row newsItem">
+			<div class="col-lg-3 newsImg">
+				<div class="imgContainer">
+					<img class="img-responsive" src="
+					<?php echo base_url().'assets/images/uploads/news/testNews.jpg'?>" 
+					width="100px" height="100px">
+				</div>
+			</div>
+			<div class="col-lg-9">
+				<div class="row newsTitle">
+					Test News Title
+				</div>
+				<div class="row postDate">
+					31 December 2017
+				</div>
+				<div class="row newsDesc">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tincidunt, sapien sed eleifend scelerisque, ipsum nibh sodales erat, sed feugiat....</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="row newsItem">
+			<div class="col-lg-3 newsImg">
+				<div class="imgContainer">
+					<img class="img-responsive" src="
+					<?php echo base_url().'assets/images/uploads/news/testNews.jpg'?>" 
+					width="100px" height="100px">
+				</div>
+			</div>
+			<div class="col-lg-9">
+				<div class="row newsTitle">
+					Test News Title
+				</div>
+				<div class="row postDate">
+					31 December 2017
+				</div>
+				<div class="row newsDesc">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tincidunt, sapien sed eleifend scelerisque, ipsum nibh sodales erat, sed feugiat....</p>
+				</div>
+			</div>
+		</div>
+
+		<button id="btnOlder">Older</button>
+
+	</section>
+</center>
