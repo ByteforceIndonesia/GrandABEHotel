aaa6fcb3fa04134d476174845621650988876f47
Jonathan Hosea <jonathan.hosea@me.com>
2017-08-11 20:18:34 +0700

    Job Opportunity frontend finished

---
+++ application/views/job_offers.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: John Nate
+ * Date: 8/11/2017
+ * Time: 7:42 PM
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<div class="container">
+    <div class="row">
+        <section id="headerJobs">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="panelHeaderJobs">
+                        <h1>Job Opportunities</h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <br><br>
+        <section id="newsList">
+            <?php foreach($jobs as $item): ?>
+                <div class="row jobItem">
+                    <?php if($item->image): ?>
+                        <div class="col-lg-3 jobImage">
+                            <div class="imgContainer">
+                                <img class="img-responsive"
+                                     src="<?php echo base_url().'assets/images/jobs/' . $item->image?>"
+                                     width="250px">
+                            </div>
+                        </div>
+                    <?php else: ?>
+                        <div class="col-lg-3">
+                            &nbsp
+                        </div>
+                    <?php endif; ?>
+                    <div class="col-lg-9" style="padding-left: 50px">
+                        <div class="row jobTitle">
+                            <h2><?php echo $item->title ?></h2>
+                        </div>
+                        <div class="row postDate">
+                            <strong><?php echo $item->created ?></strong>
+                        </div>
+                        <br>
+                        <div class="row jobDesc">
+                            <p>
+                                <?php echo $item->content ?>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <br><br>
+            <?php endforeach; ?>
+        </section>
+    </div>
+</div>
